ce2eacc7917f9cf3ba2c36589d23e48541852e53
Landon Conover <landon.conover@gmail.com>
2022-09-06 15:50:17 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+// initial todos
+// DO NOT EDIT THIS ARRAY
+// You may add props to objects if needed.
+let todos = [
+  {
+    todoID: 0,
+    todoText: "Finish Homework",
+    todoComplete: false
+  },
+  {
+    todoID: 0,
+    todoText: "Walk the dog",
+    todoComplete: true
+  },
+  {
+    todoID: 0,
+    todoText: "Clean my room",
+    todoComplete: false
+  }
+];
